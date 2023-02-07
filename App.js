b9da79dec3cdc56c0fe7f58c99160acdcf9a318e
Rohit <makawanarohit70@gmail.com>
2023-02-07 17:28:25 +0530

    FlatList Items

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import MenuItem from "./Components/MenuItem";
 import Welcome from "./Components/Welcome";
 
 export default function App() {
@@ -9,7 +10,8 @@ export default function App() {
     <>
       <View style={{ flex: 1, backgroundColor: "#495E57" }}>
         <Header />
-        <Welcome />
+        {/* <Welcome /> */}
+        <MenuItem />
       </View>
       <View style={{ backgroundColor: "#495E57" }}>
         <Footer />
